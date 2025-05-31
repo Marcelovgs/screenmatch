574a15a7e751dc6a05f8fe0396023bea6d447734
Marcelo Varela <marcelovgs12@gmail.com>
2025-05-31 12:12:13 -0300

    Adicionando dados dos episodios e dados das temporadas

--- src/test/java/oracle/projetoalura/ProjetoAluraApplicationTests.java
+++ src/test/java/oracle/projetoalura/ProjetoAluraApplicationTests.java
@@ -1,12 +1,17 @@
 package oracle.projetoalura;
 
+import oracle.projetoalura.Model.DadosEpisodio;
 import oracle.projetoalura.Model.DadosSerie;
+import oracle.projetoalura.Model.DadosTemporada;
 import oracle.projetoalura.Service.ConsumoApi;
 import oracle.projetoalura.Service.ConverteDados;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 public class ProjetoAluraApplicationTests implements CommandLineRunner {
@@ -23,5 +28,17 @@ public class ProjetoAluraApplicationTests implements CommandLineRunner {
         ConverteDados conversor = new ConverteDados();
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
+        json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=41c18d96&t=Gilmore+Girls&Season=1&Episode=2");
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+        System.out.println(dadosEpisodio);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=41c18d96&t=Gilmore+Girls&Season=" + i);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
     }
 }
